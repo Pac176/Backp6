acc29d63ffece9d3d277d34f03a7583c33e5e8d2
Godbille Pascal <pascal.godbille@orange.fr>
2020-12-15 11:33:13 +0100

    test premiere post / eslint global / models/thing

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const Thing = require('./models/thing');
 const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://Pascal:openclasserooms@cluster0.boxdf.mongodb.net/Sauces?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,32 +21,20 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-
 app.post('/api/sauces', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
   });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/sauces', (req, res, next) => {
-  const sauces = [
-    {
-      userId: 'Mon premier objet',
-      name: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      manufacturer: 'heinz',
-      description: 'qsomihvqios',
-      mainPepper: '',
-      imageUrl: '',
-      heat: 2,
-      likes: 20,
-      dislikes: 2,
-      usersLiked: [''],
-      usersDisliked: ['']
-    }
-  ];
-  res.status(200).json(sauces);
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
