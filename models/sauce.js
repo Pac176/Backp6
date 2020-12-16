2481baa001ba1f5d7d0cc1cc0bbbca91f6479684
Godbille Pascal <pascal.godbille@orange.fr>
2020-12-16 18:18:50 +0100

    correction modell sauce

--- models/sauce.js
+++ models/sauce.js
@@ -8,7 +8,6 @@ const sauceSchema = mongoose.Schema({
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number, required: true },
   dislikes: { type: Number, required: true },
