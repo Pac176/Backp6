7d5169f86e4e86b3d4e0056e07c101405e665e39
Godbille Pascal <pascal.godbille@orange.fr>
2020-12-17 11:24:30 +0100

    delete ok

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -26,7 +26,7 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(Sauce => {
+    .then(sauce => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
