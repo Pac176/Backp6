fc8216868bb5673238573bab594a9137eef9cd3b
Godbille Pascal <pascal.godbille@orange.fr>
2021-01-05 16:43:50 +0100

    async await sur sauce controller

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,131 +1,190 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
 const httpStatus = require('http-status');
-// const sauce = require('../models/sauce');
-// const user = require('../models/user');
+const badRequestMessage = { message: 'Requete non autorisée' };
+const internalServerErrorMessage = { message: 'internal Server Error' };
 
-exports.createSauce = (req, res, next) => {
+exports.createSauce = async (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${
+      req.file.filename
+    }`
   });
-
-  sauce.save()
-    .then(() => res.status(httpStatus.CREATED).json({ Message: 'Objet enregistré !' }))
-    .catch(error => {
-      res.status(httpStatus.BAD_REQUEST).json({ error });
-      fs.unlink(`images/${req.file.filename}`, (err) => {
-        if (err) throw err;
-        console.log(`${req.file.filename} was deleted`);
-      });
+  try {
+    await sauce.save();
+    res.status(httpStatus.CREATED).json({ Message: 'Objet enregistré !' });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(badRequestMessage);
+    fs.unlink(`images/${req.file.filename}`, (err) => {
+      if (err) throw err;
+      console.log(`${req.file.filename} was deleted`);
     });
+  }
 };
 
-exports.modifySauce = (req, res, next) => {
+exports.modifySauce = async (req, res, next) => {
   const sauceObject = req.file
     ? {
         ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${
+          req.file.filename
+        }`
       }
     : { ...req.body };
 
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(httpStatus.OK).json({ Message: 'Objet modifié !' }))
-    .catch(error => res.status(httpStatus.BAD_REQUEST).json({ error }));
+  try {
+    const sauce = await Sauce.findOne({ _id: req.params.id });
+    const filename = sauce.imageUrl.split('/images/')[1];
+    fs.unlink(`images/${filename}`, async () => {
+      try {
+        await Sauce.updateOne(
+          { _id: req.params.id },
+          { ...sauceObject, _id: req.params.id }
+        );
+        res.status(httpStatus.OK).json({ Message: 'Objet modifié !' });
+      } catch (error) {
+        (error) => {
+          console.log(error.message);
+          res.status(httpStatus.BAD_REQUEST).json(badRequestMessage);
+        };
+      }
+    });
+  } catch (error) {
+    error =>
+      console.log(error.message);
+    fs.unlink(`images/${req.file.filename}`, () => {
+      console.log('Suppression fichier effectuée!');
+    });
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+    ;
+  }
 };
 
-exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(httpStatus.OK).json({ Message: 'Objet supprimé !' }))
-          .catch(error => res.status(httpStatus.BAD_REQUEST).json({ error }));
-      });
-    })
-    .catch(error => res.status(500).json({ error }));
+exports.deleteSauce = async (req, res, next) => {
+  try {
+    const sauce = await Sauce.findOne({ _id: req.params.id });
+    const filename = sauce.imageUrl.split('/images/')[1];
+    fs.unlink(`images/${filename}`, async () => {
+      try {
+        await Sauce.deleteOne({ _id: req.params.id });
+        res.status(httpStatus.OK).json({ Message: 'Objet supprimé !' });
+      } catch (error) {
+        console.log(error);
+        res.status(httpStatus.BAD_REQUEST).json(badRequestMessage);
+      }
+    });
+  } catch (error) {
+    error =>
+      console.log(error.message);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(internalServerErrorMessage);
+  }
 };
 
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne()
-    .then(sauces => res.status(httpStatus.OK).json(sauces))
-    .catch(error => res.status(httpStatus.BAD_REQUEST).json({ error }));
+exports.getOneSauce = async (req, res, next) => {
+  try {
+    const oneSauce = await Sauce.findOne();
+    res.status(httpStatus.OK).json(oneSauce);
+  } catch (error) {
+    error => {
+      console.log(error.message);
+      res.status(httpStatus.BAD_REQUEST).json(badRequestMessage);
+    };
+  }
 };
-exports.getAllSauce = (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(httpStatus.OK).json(sauces))
-    .catch(error => res.status(httpStatus.BAD_REQUEST).json({ error }));
+exports.getAllSauce = async (req, res, next) => {
+  try {
+    const allSauces = await Sauce.find();
+    res.status(httpStatus.OK).json(allSauces);
+  } catch (error) {
+    error => {
+      console.log(error.message);
+      res.status(httpStatus.BAD_REQUEST).json(badRequestMessage);
+    };
+  }
 };
 
-exports.likeSauce = (req, res, next) => {
+exports.likeSauce = async (req, res, next) => {
   const cancelOpinionMessage = { message: 'Ton avis a bien été annulé' };
   const successOpinionMessage = { message: 'Ton avis a été pris en compte!' };
-  const badRequestMessage = { message: 'Requete non autorisée' };
-  const fordiddenRepetitionMessage = { message: 'Vote deja pris en compte, annule ton vote pour changer' };
+  const fordiddenRepetitionMessage = {
+    message: 'Vote deja pris en compte, annule ton vote pour changer'
+  };
   const pushDislike = { usersDisliked: req.body.userId };
   const pushLike = { usersLiked: req.body.userId };
+
   function responseBadRequest (message) {
-    res.status(httpStatus.BAD_REQUEST).json(message);
+    return res.status(httpStatus.BAD_REQUEST).json(message);
   }
-  function push (increment, likeOrDislike) {
-    Sauce.updateOne({ _id: req.params.id },
+  /* factorisation de la fonction addopinion */
+  function addOpinion (increment, likeOrDislike) {
+    Sauce.updateOne(
+      { _id: req.params.id },
       {
         $inc: increment,
         $push: likeOrDislike
-      })
+      }
+    )
       .then(() => res.status(httpStatus.OK).json(successOpinionMessage))
       .catch(() => responseBadRequest(fordiddenRepetitionMessage));
   }
-  function pull (increment, likeOrDislike) {
-    Sauce.updateOne({ _id: req.params.id },
+  /* factorisation de la fonction deleteopinion */
+  function deleteOpinion (increment, likeOrDislike) {
+    Sauce.updateOne(
+      { _id: req.params.id },
       {
         $inc: increment,
         $pull: likeOrDislike
-      })
+      }
+    )
       .then(() => res.status(httpStatus.OK).json(cancelOpinionMessage))
       .catch(() => responseBadRequest(badRequestMessage));
   }
+  /* factorisation de findOne */
 
-  switch (req.body.like) {
-    case 0:
-      Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-          if (sauce.usersLiked.includes(req.body.userId)) {
-            pull({ likes: -1 }, pushLike);
-          } else if (sauce.usersDisliked.includes(req.body.userId)) {
-            pull({ dislikes: -1 }, pushDislike);
-          } else {
-            responseBadRequest(badRequestMessage);
-          }
-        });
-      break;
-    case 1:
-      Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-          if (!(sauce.usersLiked.includes(req.body.userId)) && (!(sauce.usersDisliked.includes(req.body.userId)))) {
-            push({ likes: +1 }, pushLike);
-          } else {
-            responseBadRequest(fordiddenRepetitionMessage);
-          }
-        })
-        .catch(() => responseBadRequest(fordiddenRepetitionMessage));
-      break;
-    case -1:
-      Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-          if (!(sauce.usersLiked.includes(req.body.userId)) && (!(sauce.usersDisliked.includes(req.body.userId)))) {
-            push({ dislikes: +1 }, pushDislike);
-          } else {
-            responseBadRequest(fordiddenRepetitionMessage);
-          }
-        })
-        .catch(() => responseBadRequest(badRequestMessage));
-      break;
-    default:
-      responseBadRequest(badRequestMessage);
-      break;
+  try {
+    const currentSauce = await Sauce.findOne({ _id: req.params.id });
+    switch (req.body.like) {
+      case 0:
+        if (currentSauce.usersLiked.includes(req.body.userId)) {
+          deleteOpinion({ likes: -1 }, pushLike);
+        } else if (currentSauce.usersDisliked.includes(req.body.userId)) {
+          deleteOpinion({ dislikes: -1 }, pushDislike);
+        } else {
+          responseBadRequest(badRequestMessage);
+        }
+        break;
+      case 1:
+        if (
+          !currentSauce.usersLiked.includes(req.body.userId) &&
+          !currentSauce.usersDisliked.includes(req.body.userId)
+        ) {
+          addOpinion({ likes: +1 }, pushLike);
+        } else {
+          responseBadRequest(fordiddenRepetitionMessage);
+        }
+        break;
+      case -1:
+        if (
+          !currentSauce.usersLiked.includes(req.body.userId) &&
+          !currentSauce.usersDisliked.includes(req.body.userId)
+        ) {
+          addOpinion({ dislikes: +1 }, pushDislike);
+        } else {
+          responseBadRequest(fordiddenRepetitionMessage);
+        }
+        break;
+      default:
+        console.log('Tentative de contournement de la requete like');
+        responseBadRequest(badRequestMessage);
+        break;
+    }
+  } catch (error) {
+    console.log('Cannot get OneSauce', error);
+    responseBadRequest(fordiddenRepetitionMessage);
   }
 };
