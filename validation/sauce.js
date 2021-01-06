dfa16fb265cb6f7d8c2a6e0aee0dd5a835084ac0
Godbille Pascal <pascal.godbille@orange.fr>
2021-01-06 15:28:21 +0100

    validation sauce joi

--- validation/sauce.js
+++ validation/sauce.js
@@ -1,29 +1,34 @@
 
-
-const { ValidationError } = require('joi');
+const { EXPECTATION_FAILED } = require('http-status');
+const httpStatus = require('http-status');
 const Joi = require('joi');
 
-module.exports = (req, res, next) => {
- 
+module.exports = async (req, res, next) => {
+  const sauce = req.body.sauce;
+  const sauceObject = JSON.parse(sauce);
   const joiSchema = Joi.object({
-    userId: Joi.string().min(30).required(),
-    name: Joi.string().min(50).required(),
-    manufacturer: Joi.string().required(),
+    userId: Joi.string().alphanum().min(30).required(),
+    name: Joi.string().max(12).required(),
+    manufacturer: Joi.string().regex(/manufacturer/).required(),
     description: Joi.string().min(15).required(),
     mainPepper: Joi.string().min(15).required(),
     imageUrl: Joi.string().required(),
     heat: Joi.number().required(),
     likes: Joi.number().default(0), // { type: Number, required: false, default: 0 },
     dislikes: Joi.number().default(0), // { type: Number, required: false, default: 0 },
-    usersLiked: Joi.array(), // [{ type: String, required: false }],
-    usersDisliked: Joi.array(), // [{ type: String, required: false }],
+    usersLiked: Joi.array().length(5), // [{ type: String, required: false }],
+    usersDisliked: Joi.array() // [{ type: String, required: false }],
+  });
+
+  try {
+    const value = await joiSchema.validateAsync(sauceObject, { abortEarly: false });
+    next();
+  } catch (error) {
+    const errorMessage = [];
+    console.log(error.annotate());
+    error.details.forEach((element) => {
+      errorMessage.push(element.message);
     });
-  const sauce = req.body.sauce;
-  const sauceObject = JSON.parse(sauce)
-
-  const { value, error } = joiSchema.validate(sauceObject, { abortEarly: false });
-  
-
-
- next();
+    res.status(EXPECTATION_FAILED).json(errorMessage);
+  }
 };
