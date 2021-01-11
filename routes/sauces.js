bfed8a740cd18698399cf1a85d8967ad75947da2
Godbille Pascal <pascal.godbille@orange.fr>
2021-01-11 12:52:55 +0100

    manque valid new sauce

--- routes/sauces.js
+++ routes/sauces.js
@@ -5,9 +5,9 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer.config');
 const datavalid = require('../validation/sauce');
 
-router.post('/', auth, multer, datavalid, sauceControllers.createSauce);
+router.post('/', auth, multer, datavalid.validNewSauce, sauceControllers.createSauce);
 router.post('/:id/like', auth, sauceControllers.likeSauce);
-router.put('/:id', auth, multer, datavalid, sauceControllers.modifySauce);
+router.put('/:id', auth, multer, /* datavalid.validUpdateSauce, */sauceControllers.modifySauce);
 router.delete('/:id', auth, sauceControllers.deleteSauce);
 router.get('/:id', auth, sauceControllers.getOneSauce);
 router.get('/', auth, sauceControllers.getAllSauce);
--- validation/sauce.js
+++ validation/sauce.js
@@ -2,12 +2,13 @@
 const { EXPECTATION_FAILED } = require('http-status');
 const Joi = require('joi');
 const fs = require('fs');
-module.exports = async (req, res, next) => {
+
+exports.validNewSauce = async (req, res, next) => {
   const sauce = req.body.sauce;
   const sauceObject = JSON.parse(sauce);
   const joiSchema = Joi.object({
     userId: Joi.string().alphanum().required(),
-    name: Joi.string().max(12).required(),
+    name: Joi.string().required(),
     manufacturer: Joi.string().required(),
     description: Joi.string().required(),
     mainPepper: Joi.string().required(),
@@ -16,10 +17,46 @@ module.exports = async (req, res, next) => {
     likes: Joi.number().default(0), // { type: Number, required: false, default: 0 },
     dislikes: Joi.number().default(0), // { type: Number, required: false, default: 0 },
     usersLiked: Joi.array(), // [{ type: String, required: false }],
-    usersDisliked: Joi.array() // [{ type: String, required: false }],
+    usersDisliked: Joi.array(), // [{ type: String, required: false }],
   });
   try {
-    const valid = await joiSchema.validateAsync(sauceObject, { abortEarly: false });
+    const valid = await joiSchema.validateAsync(sauceObject, {
+      abortEarly: false,
+    });
+    next();
+  } catch (error) {
+    fs.unlink(`images/${req.file.filename}`, (err) => {
+      if (err) throw err;
+      console.log(`${req.file.filename} was deleted`);
+    });
+    const errorMessage = [];
+    console.log(error.annotate());
+    error.details.forEach((element) => {
+      errorMessage.push(element.message);
+    });
+    res.status(EXPECTATION_FAILED).json(errorMessage);
+  }
+};
+exports.validUpdateSauce = async (req, res, next) => {
+  const sauce = req.body
+  const sauceObject = JSON.parse(sauce);
+  const joiSchema = Joi.object({
+    userId: Joi.string().alphanum(),
+    name: Joi.string(),
+    manufacturer: Joi.string(),
+    description: Joi.string(),
+    mainPepper: Joi.string(),
+    imageUrl: Joi.string(),
+    heat: Joi.number(),
+    likes: Joi.number(),
+    dislikes: Joi.number(),
+    usersLiked: Joi.array(), 
+    usersDisliked: Joi.array()
+  });
+  try {
+    const valid = await joiSchema.validateAsync(sauceObject, {
+      abortEarly: false,
+    });
     next();
   } catch (error) {
     fs.unlink(`images/${req.file.filename}`, (err) => {
