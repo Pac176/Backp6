0e402be39d899704fc1b7ae2d01295f771a5fd8c
Godbille Pascal <pascal.godbille@orange.fr>
2021-01-11 10:42:12 +0100

    sans auth et datavalid

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const sauceControllers = require('../controllers/sauces');
-const auth = require('../middleware/auth');
+//const auth = require('../middleware/auth');
 const multer = require('../middleware/multer.config');
 const datavalid = require('../validation/sauce');
 
-router.post('/', auth, multer, datavalid, sauceControllers.createSauce);
-router.post('/:id/like', auth, sauceControllers.likeSauce);
-router.put('/:id', auth, multer, datavalid, sauceControllers.modifySauce);
-router.delete('/:id', auth, sauceControllers.deleteSauce);
-router.get('/:id', auth, sauceControllers.getOneSauce);
-router.get('/', auth, sauceControllers.getAllSauce);
+router.post('/', /* auth, */ multer, /* datavalid, */ sauceControllers.createSauce);
+router.post('/:id/like',/*  auth, */ sauceControllers.likeSauce);
+router.put('/:id', /* auth, */ multer, /* datavalid, */ sauceControllers.modifySauce);
+router.delete('/:id',/*  auth, */ sauceControllers.deleteSauce);
+router.get('/:id', /* auth, */ sauceControllers.getOneSauce);
+router.get('/', /* auth, */ sauceControllers.getAllSauce);
 
 module.exports = router;
